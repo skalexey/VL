a1de9dfa42500cb892cfca717514cc0252fdacb2
skalexey <skorohodov-ad@narod.ru>
2022-01-10 15:36:39 +0300

    dependencies resolution

--- JSONConverter/build.bat
+++ JSONConverter/build.bat
@@ -1,5 +1,17 @@
-mklink /D VL ..
-mkdir Build
+@ECHO off
+
+set build=Build\
+set deps=dependencies\
+
+IF not exist %deps% ( mkdir %deps% )
+
+IF not exist %deps%\rapidjson (
+	cd %deps%
+	git clone https://github.com/Tencent/rapidjson.git
+	cd ..
+)
+
+IF not exist %build% ( mkdir %build% )
 cd Build
 cmake .. -DJSON_TESTS=ON
 cmake --build .
