2a7cf743f8635702759ccc4b03b7dac245c1e517
skalexey <skorohodov-ad@narod.ru>
2022-03-05 10:22:07 +0300

    Extend Log.h (Title prefix, custom logger)

--- include/Log.h
+++ include/Log.h
@@ -4,20 +4,37 @@
 
 #ifdef LOG_ON
 	#include <iostream>
+	#define LOG_TITLE(title) namespace { const char* logTitle = title; }
+	#define LOG_STREAM(s) namespace {auto logStream = s;}
+	#define SET_LOG_VERBOSE(boolVal) namespace {bool verboseLevel = boolVal;}
+	#define LOCAL_LOG(msg) logStream() << "[" << logTitle << "] " << msg
+	#define LOCAL_ERROR(msg) logStream() << "[" << logTitle << "] " << "Error! " << msg << "\n"
+	#define LOCAL_WARNING(msg) logStream() << "[" << logTitle << "] " << "Warning: " << msg << "\n"
+	#define LOCAL_INFO(msg) logStream() << "[" << logTitle << "] " << msg << "\n"
+	#define LOCAL_VERBOSE(msg) if (verboseLevel) { logStream() << "[" << logTitle << "] " << "[V] " << msg << "\n"; }
+	#ifdef LOG_LEVEL_VERBOSE
+		#define LOG_VERBOSE(msg) std::cout << "[V] " << msg << "\n"
+	#else
+		#define LOG_VERBOSE(msg)
+	#endif
 	#define LOG(msg) std::cout << msg
 	#define LOG_ERROR(msg) std::cout << "Error! " << msg << "\n"
 	#define LOG_WARNING(msg) std::cout << "Warning: " << msg << "\n"
 	#define LOG_INFO(msg) std::cout << msg << "\n"
-#ifdef LOG_LEVEL_VERBOSE
-	#define LOG_VERBOSE(msg) std::cout << "[V] " << msg << "\n"
 #else
-	#define LOG_VERBOSE(msg)
-#endif
-#else
+	#define LOG_TITLE(title)
+	#define LOG_STREAM(s)
+	#define SET_LOG_VERBOSE(boolVal)
+	#define LOCAL_LOG(msg)
+	#define LOCAL_WARNING(msg)
+	#define LOCAL_ERROR(msg)
+	#define LOCAL_INFO(msg)
+	#define LOCAL_VERBOSE(msg)
 	#define LOG(msg)
 	#define LOG_WARNING(msg)
 	#define LOG_ERROR(msg)
 	#define LOG_INFO(msg)
 	#define LOG_VERBOSE(msg)
 #endif
+
 
