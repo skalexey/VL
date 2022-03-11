a8010bd1bc8fc59a65f8c54777bbacb0d69e3049
skalexey <skorohodov-ad@narod.ru>
2022-03-11 06:42:35 +0300

    Log.h support debug level

--- include/Log.h
+++ include/Log.h
@@ -8,6 +8,7 @@
 	#define LOG_STREAM(s) namespace {auto logStream = s;}
 	#define SET_LOG_VERBOSE(boolVal) namespace {bool verboseLevel = boolVal;}
 	#define LOCAL_LOG(msg) logStream() << "[" << logTitle << "] " << msg
+	#define LOCAL_DEBUG(msg) logStream() << "[" << logTitle << "] [D]" << msg << "\n"
 	#define LOCAL_ERROR(msg) logStream() << "[" << logTitle << "] " << "Error! " << msg << "\n"
 	#define LOCAL_WARNING(msg) logStream() << "[" << logTitle << "] " << "Warning: " << msg << "\n"
 	#define LOCAL_INFO(msg) logStream() << "[" << logTitle << "] " << msg << "\n"
@@ -18,6 +19,7 @@
 		#define LOG_VERBOSE(msg)
 	#endif
 	#define LOG(msg) std::cout << msg
+	#define LOG_DEBUG(msg) std::cout << msg
 	#define LOG_ERROR(msg) std::cout << "Error! " << msg << "\n"
 	#define LOG_WARNING(msg) std::cout << "Warning: " << msg << "\n"
 	#define LOG_INFO(msg) std::cout << msg << "\n"
@@ -26,11 +28,13 @@
 	#define LOG_STREAM(s)
 	#define SET_LOG_VERBOSE(boolVal)
 	#define LOCAL_LOG(msg)
+	#define LOCAL_DEBUG(msg)
 	#define LOCAL_WARNING(msg)
 	#define LOCAL_ERROR(msg)
 	#define LOCAL_INFO(msg)
 	#define LOCAL_VERBOSE(msg)
 	#define LOG(msg)
+	#define LOG_DEBUG(msg)
 	#define LOG_WARNING(msg)
 	#define LOG_ERROR(msg)
 	#define LOG_INFO(msg)
