475279ba80baa87055c6857ac2305f06ce49b249
skalexey <skorohodov-ad@narod.ru>
2022-05-12 10:33:22 +0300

    Scripts update: added log function, args parsing moved to build_utils script

--- JSONConverter/deps_scenario.sh
+++ JSONConverter/deps_scenario.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 source dependencies.sh
 source deps_config.sh
 
 download_dependency "VL" "$depsLocation" "git@github.com:skalexey/VL.git"
-cd "VL"
-
 download_dependency "rapidjson" "$depsLocation" "https://github.com/Tencent/rapidjson.git"
 
--- deps_scenario.sh
+++ deps_scenario.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 source dependencies.sh
 source deps_config.sh
