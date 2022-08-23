7437187ed33e3e1ed5398aac6211f4c62f010807
skalexey <skorohodov-ad@narod.ru>
2022-08-23 15:40:55 +0300

    Nested dependencies auto resolution.

--- deps_scenario.sh
+++ deps_scenario.sh
@@ -6,6 +6,7 @@ deps_scenario()
 	source deps_config.sh
 
 	download_dependency "Utils" "$depsLocation" "git@github.com:skalexey/Utils.git"
+	source "$depsLocation/VL/JSONConverter/deps_scenario.sh"
 }
 
 deps_scenario $@
