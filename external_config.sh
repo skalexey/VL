7520058fc33c6c34a69d25051cd32885ecdc2000
Alex <alexeiskorokhodov@gmail.com>
2022-09-04 12:06:00 +0300

    Update CMakeLists. Update deps configuration.

--- external_config.sh
+++ external_config.sh
@@ -3,7 +3,7 @@ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $THIS_DIR/os.sh
 
 if is_windows; then
-	export JSONConverter_deps="${HOME}/Projects"
+	export vl_deps="${HOME}/Projects"
 else
-	export JSONConverter_deps="${HOME}/Projects"
+	export vl_deps="${HOME}/Projects"
 fi
