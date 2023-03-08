13e849952746490bd411ad08e561e5b51e9abf30
skalexey <skorohodov-ad@narod.ru>
2023-03-08 15:59:11 +0100

    Use local variables.

--- external_config.sh
+++ external_config.sh
@@ -1,9 +1,14 @@
 
-THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source $THIS_DIR/os.sh
+function job()
+{
+	local THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+	source $THIS_DIR/os.sh
 
-if is_windows; then
-	export vl_deps="${HOME}/Projects"
-else
-	export vl_deps="${HOME}/Projects"
-fi
+	if is_windows; then
+		export vl_deps="${HOME}/Projects"
+	else
+		export vl_deps="${HOME}/Projects"
+	fi
+}
+
+job $@
